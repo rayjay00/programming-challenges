15b8b6113bbad03118b7bf2be2260fc83b54a63a
Ryan <hello@ryanjordal.me>
2019-02-08 23:42:04 -0500

    added square digits

--- JavaScript Challenges/February_2018.js
+++ JavaScript Challenges/February_2018.js
@@ -8,5 +8,13 @@ function average(scores) {
 // Complete the function/method so that it returns the url with anything after the anchor (#) removed.
 function removeUrlAnchor(url){
     return url.split("#")[0];
+}
+
+//In this kata, you are asked to square every digit of a number.
+//For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+//Note: The function accepts an integer and returns an integer
+
+function squareDigits(num){
+    return Number(num.toString().split("").map(num => num * num).join(""));
   }
 
