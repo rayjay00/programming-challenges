641171758fa876ecb72f4ee9a2cd7fa368f62a64
Ryan <hello@ryanjordal.me>
2019-02-09 00:50:46 -0500

    odd or even

--- JavaScript Challenges/February_2018.js
+++ JavaScript Challenges/February_2018.js
@@ -36,3 +36,20 @@ function highAndLow(numbers){
     return `${numsSorted[0]} ${numsSorted[numsSorted.length - 1]}`;
 }
 
+// Given an array of numbers, determine whether the sum of all of the numbers is odd or even.
+// Give your answer in string format as 'odd' or 'even'.
+// If the input array is empty consider it as: [0] (array with a zero).
+
+function oddOrEven(array) {
+    if (array.length === 0) {
+        return 'even'
+     } else {
+        const addedArr = array.reduce((i, num) => i + num);
+        if ((addedArr % 2 === 0)) {
+            return 'even';
+        } else {
+            return 'odd';
+        }
+    }
+ }
+
