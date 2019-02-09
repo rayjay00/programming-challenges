1dba7cf389a4ece059f3be8b953f4eee98bf85a1
Ryan <hello@ryanjordal.me>
2019-02-09 00:30:35 -0500

    high and low nums

--- JavaScript Challenges/February_2018.js
+++ JavaScript Challenges/February_2018.js
@@ -16,5 +16,23 @@ function removeUrlAnchor(url){
 
 function squareDigits(num){
     return Number(num.toString().split("").map(num => num * num).join(""));
-  }
+}
+
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Example:
+
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes:
+
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+function highAndLow(numbers){
+    const numsSorted = numbers.split(" ").sort((a, b) => b - a);
+    return `${numsSorted[0]} ${numsSorted[numsSorted.length - 1]}`;
+}
 
