876d32e26eaf95fe378e1b9a7fcfbf669e1c4866
Ryan <hello@ryanjordal.me>
2019-02-08 23:19:27 -0500

    average using reduce

---
+++ JavaScript Challenges/February_2018.js
@@ -0,0 +1,6 @@
+// Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
+
+function average(scores) {
+    // code to calculate the average
+    return Math.round(scores.reduce((n, currentVal) => n + currentVal) / scores.length);
+}
