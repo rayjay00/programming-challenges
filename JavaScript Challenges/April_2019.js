58d1d3524cacf258ac5e1e7b3d5a43e2f3325a03
Ryan <rjordal@intuitsolutions.net>
2019-04-23 13:06:10 -0400

    new challenge

---
+++ JavaScript Challenges/April_2019.js
@@ -0,0 +1,12 @@
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+function smallEnough(a, limit) {
+  //creat an array of numbers that are lower than or equal to the "a" array's contents
+  const lowerNums = a.filter(num => {
+    return num <= limit;
+  });
+  //are their lengths equal? This will return true or false;
+  return lowerNums.length === a.length;
+}
