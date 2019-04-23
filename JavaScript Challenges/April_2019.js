a8fcbacba56cbb83db495cefd0052eed1fc63835
Ryan <rjordal@intuitsolutions.net>
2019-04-23 16:35:34 -0400

    current progress w/ palindrome

--- JavaScript Challenges/April_2019.js
+++ JavaScript Challenges/April_2019.js
@@ -2,27 +2,38 @@
 
 // You can assume all values in the array are numbers.
 
-function smallEnough(a, limit) {
-  //creat an array of numbers that are lower than or equal to the "a" array's contents
-  const lowerNums = a.filter(num => {
-    return num <= limit;
-  });
-  //are their lengths equal? This will return true or false;
-  return lowerNums.length === a.length;
-}
+// function smallEnough(a, limit) {
+//   //creat an array of numbers that are lower than or equal to the "a" array's contents
+//   const lowerNums = a.filter(num => {
+//     return num <= limit;
+//   });
+//   //are their lengths equal? This will return true or false;
+//   return lowerNums.length === a.length;
+// }
 
 const longestPalindrome = letters => {
   //make it an array i guess?
-  const lettersArr = letters.split("");
+  const mainArr = letters.split("");
+  const lettersArr = mainArr.reverse();
   //start checking if something is equal or not
-  letters.forEach(letter => {
-    if (letter)
-  })
+  // add all possible arrays with lengths to its own array
+  const matchArr = lettersArr.map((letter, index) => {
+    return lettersArr.slice(index, lettersArr.length);
+  });
+  console.log("matchArr", matchArr);
+  const reversedArrTest = matchArr.filter(arr => {
+    console.log("arr inside", arr);
+    return arr.toString() == arr.reverse().toString();
+  });
+  return reversedArrTest;
 };
 
-longestPalindrome("baablkj12345432133d");
+longestPalindrome("baablk");
 
 // split into array at ""
 // start at 0, go through array and see if the selection joined is equal to joined reverse
+// update: use slice
+// expected output: Array ["bison", "camel", "duck", "elephant"]
 // if so, return the length of that after maybe joining them?
-// 
+//
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
