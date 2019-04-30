c257bbbb23161ab0a28947338865cba17899f090
Ryan <rjordal@intuitsolutions.net>
2019-04-30 12:03:27 -0400

    added simple sort challenge

--- JavaScript Challenges/April_2019.js
+++ JavaScript Challenges/April_2019.js
@@ -26,3 +26,18 @@ longestPalindrome("baablkj12345432133d");
 // start at 0, go through array and see if the selection joined is equal to joined reverse
 // if so, return the length of that after maybe joining them?
 // 
+
+
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 integers. No floats or empty arrays will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumTwoSmallestNumbers(numbers) {  
+  const sortedArr = numbers.sort((a, b) => a - b);
+
+  const twoLowestAdded = sortedArr[0] + sortedArr[1];
+  
+  return twoLowestAdded;
+};
